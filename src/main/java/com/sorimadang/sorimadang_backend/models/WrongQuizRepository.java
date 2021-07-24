b4ace22aa9343c6e767f8c7ac69696e964fbbc5b
권민정 <22alswjd@naver.com>
2021-07-25 05:36:40 +0900

    add login check function

--- src/main/java/com/sorimadang/sorimadang_backend/models/WrongQuizRepository.java
+++ src/main/java/com/sorimadang/sorimadang_backend/models/WrongQuizRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface WrongQuizRepository extends JpaRepository<WrongQuiz, WrongQuizKeys> {
+public interface WrongQuizRepository extends JpaRepository<WrongQuiz, String> {
     List<WrongQuiz> findAllByUserId(String id);
 }
